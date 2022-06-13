aba1d9e408f7798177338a302c5784e40f1aa4d2
Endrit Bytyqi <endrit.bytyqi@elba-tech.com>
2022-06-13 13:15:39 +0200

    add main

--- wikihuh/lib/main.dart
+++ wikihuh/lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const Wikihuh());
+}
+
 class Wikihuh extends StatelessWidget {
-  const Wikihuh({ Key? key }) : super(key: key);
+  const Wikihuh({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text(
-        "Wikihuh"
+    return const MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: Text("Wikihuh"),
+        ),
       ),
     );
   }
