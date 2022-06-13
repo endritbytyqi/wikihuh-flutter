acbfe63ca6eda66fdfecad10f6494c05919e58c5
Endrit Bytyqi <endrit.bytyqi@elba-tech.com>
2022-06-13 13:05:50 +0200

    initial commit

---
+++ wikihuh/android/app/src/main/kotlin/com/example/wikihuh/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wikihuh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ wikihuh/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Wikihuh extends StatelessWidget {
+  const Wikihuh({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text(
+        "Wikihuh"
+      ),
+    );
+  }
+}
